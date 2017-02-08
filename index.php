b9ca9edb311824213fb258ebdb9e154409fe31a5
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-08 23:13:43 +0000

    refactor and document

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 
+// add $parentpath parameter
 function create_array(array $elements, $parentId = 0, $parentpath = '') {
 
     $branch = array();
 
     foreach ($elements as $element) {
-        $element['fullpath'] = $fullpath = "$parentpath/$element[menu_nl]";
+        // set the fullpath from parent concatenated to this menu_nl
+        $element['fullpath'] = "$parentpath/$element[menu_nl]";
 
         if ($element['parent_id'] == $parentId) {
 
-            $children = create_array($elements, $element['id'], $fullpath);
+            // pass the fullpath down to it's children
+            $children = create_array($elements, $element['id'], $element['fullpath']);
 
             if ($children) {
                 $element[$element['menu_nl']] = $children;
