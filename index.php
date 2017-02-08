0eda80ad18bb557b59fc599b7f455fc87dd87d9e
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-08 22:57:54 +0000

    pass full path down

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
-function create_array(array $elements, $parentId = 0) {
+function create_array(array $elements, $parentId = 0, $parentpath = '') {
 
     $branch = array();
 
     foreach ($elements as $element) {
+        $element['fullpath'] = $fullpath = "$parentpath/$element[menu_nl]";
+
         if ($element['parent_id'] == $parentId) {
 
-
-            $children = create_array($elements, $element['id']);
+            $children = create_array($elements, $element['id'], $fullpath);
 
             if ($children) {
                 $element[$element['menu_nl']] = $children;
             }
-
 
             $branch[] = $element;
         }
